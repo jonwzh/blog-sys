4729a7535e642b62154ab0ea0ad45aafd70acad2
Jonathan Wang <jonwzh@gmail.com>
2019-11-19 21:32:32 -0800

    Add a simple login system

--- controller/home.go
+++ controller/home.go
@@ -10,10 +10,49 @@ type home struct{}
 
 func (h home) registerRoutes() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/login", loginHandler)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	vop := vm.IndexViewModelOp{}
 	v := vop.GetVM()
-	templates["index.html"].Execute(w, &v)
+	templates["index.html"].Execute(w, v)
+}
+
+func check(username, password string) bool {
+	if username == "jonwzh" && password == "123123" {
+		return true
+	}
+	return false
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	tpName := "login.html"
+	vop := vm.LoginViewModelOp{}
+	v := vop.GetVM()
+	if r.Method == http.MethodGet {
+		templates[tpName].Execute(w, v)
+	}
+	if r.Method == http.MethodPost {
+		r.ParseForm()
+		username := r.Form.Get("username")
+		password := r.Form.Get("password")
+		if len(username) < 3 {
+			v.AddError("Username must be longer than 3 chars")
+		}
+
+		if len(password) < 6 {
+			v.AddError("Password must be longer than 6 chars")
+		}
+
+		if !check(username, password) {
+			v.AddError("Username or password is not correct, please try again")
+		}
+
+		if len(v.Errs) > 0 {
+			templates[tpName].Execute(w, &v)
+		} else {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+		}
+	}
 }
